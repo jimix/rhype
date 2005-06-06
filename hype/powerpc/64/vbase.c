cb7f8f60aa6f886752394bba0fba2a8bf58dd92f
mostrows <mostrows@localhost>
2005-06-06 15:58:05 +0000

    Split vbase code to move virtualized environment (exceptions, registers) to venv.c, keeping memory code in vbase.c.

--- hype/powerpc/64/vbase.c
+++ hype/powerpc/64/vbase.c
@@ -18,6 +18,8 @@
  * $Id$
  */
 
+/* Implementation of virtualized base memory */
+
 #include <config.h>
 #include <types.h>
 #include <cpu_thread.h>
@@ -82,7 +84,6 @@ typedef	sval (*hcall_fn_t)(struct cpu_thread* thr, uval a2, uval a3, uval a4,
 
 sval xh_kern_slb(struct cpu_thread* thread, uval addr,
 		 struct vexc_save_regs *vr);
-sval xh_kern_prog_ex(struct cpu_thread* thread, uval addr);
 sval xh_kern_pgflt(struct cpu_thread* thread, uval addr,
 		   struct vexc_save_regs *vr);
 
@@ -243,7 +244,6 @@ xh_kern_pgflt(struct cpu_thread* thr, uval type, struct vexc_save_regs *vr)
 
 	pte.bits.rpn = ra >> LOG_PGSIZE;
 
-
 	sval ret = insert_ea_map(thr, vsid, addr, 1, pte);
 	if (ret == H_Success) {
 		++vmc->vmc_num_ptes;
@@ -298,6 +298,7 @@ xh_kern_slb(struct cpu_thread* thread, uval type, struct vexc_save_regs *vr)
 
 	if (!vmc) {
 		hprintf("No vm_class for 0x%lx\n", addr);
+		breakpoint();
 		return insert_debug_exception(thread, V_DEBUG_MEM_FAULT);
 	}
 
@@ -339,330 +340,5 @@ xh_syscall(uval a1, uval a2, uval a3, uval a4, uval a5, uval a6,
 	ret = hcall_fn(thread, a2, a3, a4, a5, a6, a7, a8, a9, a10);
 	tca->save_area->reg_gprs[3] = ret;
 	return ret;
-}
-
-inline void
-set_v_msr(struct cpu_thread* thr, uval val)
-{
-	if ((val ^ thr->vregs->v_msr) & MSR_PR) {
-		if (val & MSR_PR) {
-			vmc_exit_kernel(thr);
-			clear_bit(thr->vstate.thread_mode, VSTATE_KERN_MODE);
-		} else {
-			vmc_enter_kernel(thr);
-			set_bit(thr->vstate.thread_mode, VSTATE_KERN_MODE);
-		}
-	}
-
-	thr->vregs->v_msr = (val & ~(MSR_HV|(MSR_SF>>2))) | MSR_AM;
-
-	if (! test_bit(thr->vregs->v_msr, MSR_EE)) {
-		clear_bit(thr->vstate.thread_mode, VSTATE_FORCE_EXCEPTION);
-		return;
-	}
-
-	if (test_bit(thr->vstate.thread_mode, VSTATE_PENDING)) {
-		set_bit(thr->vstate.thread_mode, VSTATE_FORCE_EXCEPTION);
-	}
-}
-
-static inline void
-mtgpr(struct cpu_thread* thr, uval gpr, uval val)
-{
-	switch (gpr) {
-	case 14 ... 31:
-		thr->reg_gprs[gpr] = val;
-		break;
-	case 0 ... 13:
-		get_tca()->save_area->reg_gprs[gpr] = val;
-		break;
-	}
-}
-
-static inline uval
-mfgpr(struct cpu_thread* thr, uval gpr)
-{
-	uval val;
-	switch (gpr) {
-	case 14 ... 31:
-		val = thr->reg_gprs[gpr];
-		break;
-	case 0 ... 13:
-		val = get_tca()->save_area->reg_gprs[gpr];
-		break;
-	}
-	return val;
-
-}
-
-/* Extract bits of word, start/stop indexing uses Book I/III conventions
- * (i.e. bit 0 is on the left)
- */
-static inline
-uval extract_bits(uval val, uval start, uval numbits)
-{
-	return (val >> (32 - (start + numbits))) & ((1 << numbits) - 1);
-}
-
-static sval
-decode_spr_ins(struct cpu_thread* thr, uval addr, uval32 ins)
-{
-	struct thread_control_area *tca = get_tca();
-	uval id = thr->vregs->active_vsave;
-	struct vexc_save_regs *vr = &thr->vregs->vexc_save[id];
-	sval ret = -1;
-	uval opcode = extract_bits(ins, 0, 6);
-	uval type = extract_bits(ins, 21, 10);
-	uval spr_0_4 = extract_bits(ins, 16, 5);
-	uval spr_5_9 = extract_bits(ins, 11, 5);
-	uval gpr  = extract_bits(ins, 6, 5);
-	uval spr = (spr_0_4 << 5) | spr_5_9;
-
-	sync_from_dec();
-
-	/* mfmsr */
-	if (opcode == 31 && type == 83) {
-		//hprintf("mfmsr r%ld at 0x%lx\n",gpr, addr);
-		mtgpr(thr, gpr, thr->vregs->v_msr);
-		tca->srr0 += sizeof(uval32);
-		return 0;
-	}
-
-	/* mtmsrd */
-	if (opcode == 31 && (type == 178 || type == 146)) {
-		uval64 val = mfgpr(thr, gpr);
-		//hprintf("mtmsrd r%ld <- 0x%llx at 0x%lx\n", gpr, val, addr);
-
-		uval64 chg_mask = ~0ULL;
-		uval l = extract_bits(ins, 15, 1);
-		if (type == 146) {
-			// mtmsr , 32-bits
-			chg_mask = 0xffffffff;
-		}
-
-		if (l == 1) {
-			chg_mask = (MSR_EE | MSR_RI);
-		}
-
-		/* These are the only bits we can change here */
-		val = (val & chg_mask) | (thr->vregs->v_msr & ~chg_mask);
-
-		set_v_msr(thr, val);
-		val = thr->vregs->v_msr;
-		val |= V_LPAR_MSR_ON;
-		val &= ~V_LPAR_MSR_OFF;
-		tca->srr1 = val;
-		tca->srr0 += sizeof(uval32);
-		return 0;
-	}
-
-	/* mfspr */
-#define SET_GPR(label, src)					\
-	case label: mtgpr(thr, gpr, src); break;
-
-	if (opcode == 31 && type == 339) {
-		ret = 0;
-		switch (spr) {
-			SET_GPR(SPRN_SRR0, vr->v_srr0);
-			SET_GPR(SPRN_SRR1, vr->v_srr1);
-			SET_GPR(SPRN_PVR, mfpvr());
-			SET_GPR(SPRN_PIR, mfpir());
-			SET_GPR(SPRN_DEC, thr->vregs->v_dec);
-
-		case SPRN_DSISR:
-		case SPRN_DAR:
-			mtgpr(thr, gpr, 0);
-			break;
-		case SPRN_HID0:
-		case SPRN_HID1:
-		case SPRN_HID4:
-		case SPRN_HID5:
-			mtgpr(thr, gpr, 0xdeadbeeffeedfaceULL);
-			break;
-
-		default:
-			ret = -1;
-			break;
-		}
-
-		if (ret != -1) {
-			tca->srr0 += sizeof(uval32);
-			return ret;
-		}
-
-	}
-
-#define SET_VREG(label, field)						\
-	case label: thr->vregs->field = mfgpr(thr, gpr); break;
-
-	/* mtspr */
-	if (opcode == 31 && type == 467) {
-		ret = 0;
-		switch (spr) {
-			SET_VREG(SPRN_SPRG0, v_sprg0);
-			SET_VREG(SPRN_SPRG1, v_sprg1);
-			SET_VREG(SPRN_SPRG2, v_sprg2);
-			SET_VREG(SPRN_SPRG3, v_sprg3);
-		case SPRN_DEC:
-			partition_set_dec(thr, mfgpr(thr, gpr));
-			thr->vregs->v_dec = mfgpr(thr, gpr);
-			break;
-		case SPRN_SRR0:
-			vr->v_srr0 = mfgpr(thr, gpr);
-			break;
-		case SPRN_SRR1:
-			vr->v_srr1 = mfgpr(thr, gpr);
-			break;
-
-		case SPRN_DSISR:
-		case SPRN_DAR:
-			break;
-		default:
-			ret = -1;
-			break;
-		}
-
-		if (ret != -1) {
-			tca->srr0 += sizeof(uval32);
-			return ret;
-		}
-
-	}
-
-	/* rfid */
-	if (opcode == 19 && type == 18) {
-
-		uval val = vr->v_srr1;
-
-		set_v_msr(thr, val);
-		val |= V_LPAR_MSR_ON;
-		val &= ~V_LPAR_MSR_OFF;
-		tca->srr1 = val;
-		tca->srr0 = vr->v_srr0;
-		hprintf("rfid: %lx -> %lx\n",addr, vr->v_srr0);
-
-		return 0;
-	}
-
-	if (ret == -1) {
-		hprintf("Decode instruction: %ld %ld %ld %ld\n",
-			opcode, type, spr, gpr);
-	}
-	return ret;
-}
-
-sval
-xh_kern_prog_ex(struct cpu_thread* thread, uval addr)
-{
-	uval32 ins;
-	struct thread_control_area *tca = get_tca();
-
-	sval ret = 0;
-	uval srr1 = tca->srr1;
-
-	if (! (srr1 & (1ULL << (63-45)))) goto abort;
-
-	struct vm_class *vmc = find_kernel_vmc(thread, addr);
-	if (!vmc) goto abort;
-
-	union ptel pte;
-	uval laddr = vmc_xlate(vmc, addr, &pte);
-	if (laddr == INVALID_LOGICAL_ADDRESS) goto abort;
-
-	uval pa = logical_to_physical_address(thread->cpu->os, laddr,
-					      sizeof(uval32));
-	if (pa == INVALID_PHYSICAL_ADDRESS) goto abort;
-
-	ins = *(uval32*)pa;
-
-	ret = decode_spr_ins(thread, addr, ins);
-	if (ret == 0) return -1;
-abort:
-	return insert_exception(thread, EXC_V_DEBUG);
-}
-
-
-extern sval deliver_async_exception(struct cpu_thread *thread);
-
-sval
-deliver_async_exception(struct cpu_thread *thread)
-{
-	uval *mode = &thread->vstate.thread_mode;
-	if (test_bit(*mode, VSTATE_PENDING_EXT)) {
-		clear_bit(*mode, VSTATE_PENDING_EXT);
-		return insert_exception(thread, EXC_V_EXT);
-	}
-	if (test_bit(*mode, VSTATE_PENDING_DEC)) {
-		clear_bit(*mode, VSTATE_PENDING_DEC);
-		return insert_exception(thread, EXC_V_DEC);
-	}
-
-	return -1;
-}
-
-
-sval
-insert_exception(struct cpu_thread *thread, uval exnum)
-{
-	struct thread_control_area *tca = get_tca();
-	struct vregs* vregs = tca->vregs;
-	struct vexc_save_regs* vr = tca->save_area;
-
-
-//	hprintf("Reflect exception: %ld %x\n", exnum, vr->exc_num);
-	if (!thread->vregs->exception_vectors[exnum])
-		return vregs->active_vsave;
-
-//	hprintf("%s from 0x%016lx %lx\n", __func__, tca->srr0, exnum);
-
-	vr->v_srr0 = tca->srr0;
-
-	/* Provide accurate trap bits */
-	vr->v_srr1 = vregs->v_msr | (MSR_TRAP_BITS & tca->srr1);
-
-	/* enforce HV, EE, PR off, AM on */
-	uval vmsr = vregs->v_msr;
-
-	vmsr &= ~(MSR_EE|MSR_HV|MSR_PR|MSR_IR|MSR_DR|MSR_FE0|
-		  MSR_FE1|MSR_BE|MSR_FP|MSR_PMM|MSR_SE|MSR_BE);
-	vmsr |= MSR_SF;
-//	hprintf("v_msr: 0x%016lx v_ex_msr: 0x%016lx %ld\n",
-//		vregs->v_msr, vmsr, exnum);
-	set_v_msr(thread, vmsr);
-
-	uval64 msr = vregs->v_ex_msr;
-	msr |= V_LPAR_MSR_ON;
-	msr &= ~V_LPAR_MSR_OFF;
-	tca->srr1 = msr;
-	tca->srr0 = vregs->exception_vectors[exnum];
-
-	/* Make vregs->v_dec match current dec value */
-	sync_from_dec();
-
-
-	vr->prev_vsave = vregs->active_vsave;
-	vregs->active_vsave = (vr - &vregs->vexc_save[0]);
-
-	return vregs->active_vsave;
-}
-
-
-sval
-insert_debug_exception(struct cpu_thread *thread, uval dbgflag)
-{
-	struct thread_control_area *tca = get_tca();
-
-	if (thread->vregs->debug & V_DEBUG_FLAG) {
-
-		thread->vregs->debug |= dbgflag;
-
-		/* Increment to the next instruction */
-		tca->srr0 += sizeof(uval32);
-
-		return thread->vregs->active_vsave;
-	}
-
-	return insert_exception(thread, EXC_V_DEBUG);
-
 }
 
---
+++ hype/powerpc/64/venv.c
@@ -0,0 +1,372 @@
+/*
+ * Copyright (C) 2005 Michal Ostrowski <mostrows@watson.ibm.com>, IBM Corp.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id$
+ */
+
+/* Implementation of virtualized environment */
+
+#include <config.h>
+#include <types.h>
+#include <cpu_thread.h>
+#include <pmm.h>
+#include <vregs.h>
+#include <xh.h>
+#include <bitops.h>
+#include <hype_control_area.h>
+#include <thread_control_area.h>
+#include <objalloc.h>
+#include <vm_class.h>
+#include <vm_class_inlines.h>
+#include <vdec.h>
+#include <vm.h>
+#include <bitmap.h>
+
+sval xh_kern_prog_ex(struct cpu_thread* thread, uval addr);
+
+inline void
+set_v_msr(struct cpu_thread* thr, uval val)
+{
+	if ((val ^ thr->vregs->v_msr) & MSR_PR) {
+		if (val & MSR_PR) {
+			vmc_exit_kernel(thr);
+			clear_bit(thr->vstate.thread_mode, VSTATE_KERN_MODE);
+		} else {
+			vmc_enter_kernel(thr);
+			set_bit(thr->vstate.thread_mode, VSTATE_KERN_MODE);
+		}
+	}
+
+	thr->vregs->v_msr = (val & ~(MSR_HV|(MSR_SF>>2))) | MSR_AM;
+	assert(thr->vregs->v_msr != 0x2900, "bad msr\n");
+
+	if (! test_bit(thr->vregs->v_msr, MSR_EE)) {
+		clear_bit(thr->vstate.thread_mode, VSTATE_FORCE_EXCEPTION);
+		return;
+	}
+
+	if (test_bit(thr->vstate.thread_mode, VSTATE_PENDING)) {
+		set_bit(thr->vstate.thread_mode, VSTATE_FORCE_EXCEPTION);
+	}
+}
+
+
+static inline void
+mtgpr(struct cpu_thread* thr, uval gpr, uval val)
+{
+	switch (gpr) {
+	case 14 ... 31:
+		thr->reg_gprs[gpr] = val;
+		break;
+	case 0 ... 13:
+		get_tca()->save_area->reg_gprs[gpr] = val;
+		break;
+	}
+}
+
+static inline uval
+mfgpr(struct cpu_thread* thr, uval gpr)
+{
+	uval val;
+	switch (gpr) {
+	case 14 ... 31:
+		val = thr->reg_gprs[gpr];
+		break;
+	case 0 ... 13:
+		val = get_tca()->save_area->reg_gprs[gpr];
+		break;
+	}
+	return val;
+
+}
+
+/* Extract bits of word, start/stop indexing uses Book I/III conventions
+ * (i.e. bit 0 is on the left)
+ */
+static inline
+uval extract_bits(uval val, uval start, uval numbits)
+{
+	return (val >> (32 - (start + numbits))) & ((1 << numbits) - 1);
+}
+
+static sval
+decode_spr_ins(struct cpu_thread* thr, uval addr, uval32 ins)
+{
+	struct thread_control_area *tca = get_tca();
+	uval id = thr->vregs->active_vsave;
+	struct vexc_save_regs *vr = &thr->vregs->vexc_save[id];
+	sval ret = -1;
+	uval opcode = extract_bits(ins, 0, 6);
+	uval type = extract_bits(ins, 21, 10);
+	uval spr_0_4 = extract_bits(ins, 16, 5);
+	uval spr_5_9 = extract_bits(ins, 11, 5);
+	uval gpr  = extract_bits(ins, 6, 5);
+	uval spr = (spr_0_4 << 5) | spr_5_9;
+
+	sync_from_dec();
+
+	/* mfmsr */
+	if (opcode == 31 && type == 83) {
+		//hprintf("mfmsr r%ld at 0x%lx\n",gpr, addr);
+		mtgpr(thr, gpr, thr->vregs->v_msr);
+		tca->srr0 += sizeof(uval32);
+		return 0;
+	}
+
+	/* mtmsrd */
+	if (opcode == 31 && (type == 178 || type == 146)) {
+		uval64 val = mfgpr(thr, gpr);
+		//hprintf("mtmsrd r%ld <- 0x%llx at 0x%lx\n", gpr, val, addr);
+
+		uval64 chg_mask = ~0ULL;
+		uval l = extract_bits(ins, 15, 1);
+		if (type == 146) {
+			// mtmsr , 32-bits
+			chg_mask = 0xffffffff;
+		}
+
+		if (l == 1) {
+			chg_mask = (MSR_EE | MSR_RI);
+		}
+
+		/* These are the only bits we can change here */
+		val = (val & chg_mask) | (thr->vregs->v_msr & ~chg_mask);
+
+		set_v_msr(thr, val);
+		val = thr->vregs->v_msr;
+		val |= V_LPAR_MSR_ON;
+		val &= ~V_LPAR_MSR_OFF;
+		tca->srr1 = val;
+		tca->srr0 += sizeof(uval32);
+		return 0;
+	}
+
+	/* mfspr */
+#define SET_GPR(label, src)					\
+	case label: mtgpr(thr, gpr, src); break;
+
+	if (opcode == 31 && type == 339) {
+		ret = 0;
+		switch (spr) {
+			SET_GPR(SPRN_SRR0, vr->v_srr0);
+			SET_GPR(SPRN_SRR1, vr->v_srr1);
+			SET_GPR(SPRN_PVR, mfpvr());
+			SET_GPR(SPRN_PIR, mfpir());
+			SET_GPR(SPRN_DEC, thr->vregs->v_dec);
+
+		case SPRN_DSISR:
+		case SPRN_DAR:
+			mtgpr(thr, gpr, 0);
+			break;
+		case SPRN_HID0:
+		case SPRN_HID1:
+		case SPRN_HID4:
+		case SPRN_HID5:
+			mtgpr(thr, gpr, 0xdeadbeeffeedfaceULL);
+			break;
+
+		default:
+			ret = -1;
+			break;
+		}
+
+		if (ret != -1) {
+			tca->srr0 += sizeof(uval32);
+			return ret;
+		}
+
+	}
+
+#define SET_VREG(label, field)						\
+	case label: thr->vregs->field = mfgpr(thr, gpr); break;
+
+	/* mtspr */
+	if (opcode == 31 && type == 467) {
+		ret = 0;
+		switch (spr) {
+			SET_VREG(SPRN_SPRG0, v_sprg0);
+			SET_VREG(SPRN_SPRG1, v_sprg1);
+			SET_VREG(SPRN_SPRG2, v_sprg2);
+			SET_VREG(SPRN_SPRG3, v_sprg3);
+		case SPRN_DEC:
+			partition_set_dec(thr, mfgpr(thr, gpr));
+			thr->vregs->v_dec = mfgpr(thr, gpr);
+			break;
+		case SPRN_SRR0:
+			vr->v_srr0 = mfgpr(thr, gpr);
+			break;
+		case SPRN_SRR1:
+			vr->v_srr1 = mfgpr(thr, gpr);
+			break;
+
+		case SPRN_DSISR:
+		case SPRN_DAR:
+			break;
+		default:
+			ret = -1;
+			break;
+		}
+
+		if (ret != -1) {
+			tca->srr0 += sizeof(uval32);
+			return ret;
+		}
+
+	}
+
+	/* rfid */
+	if (opcode == 19 && type == 18) {
+
+		uval val = vr->v_srr1;
+
+		set_v_msr(thr, val);
+		val |= V_LPAR_MSR_ON;
+		val &= ~V_LPAR_MSR_OFF;
+		tca->srr1 = val;
+		tca->srr0 = vr->v_srr0;
+		hprintf("rfid: %lx -> %lx\n",addr, vr->v_srr0);
+
+		return 0;
+	}
+
+	if (ret == -1) {
+		hprintf("Decode instruction: %ld %ld %ld %ld\n",
+			opcode, type, spr, gpr);
+	}
+	return ret;
+}
+
+sval
+xh_kern_prog_ex(struct cpu_thread* thread, uval addr)
+{
+	uval32 ins;
+	struct thread_control_area *tca = get_tca();
+
+	sval ret = 0;
+	uval srr1 = tca->srr1;
+
+	if (! (srr1 & (1ULL << (63-45)))) goto abort;
+
+	struct vm_class *vmc = find_kernel_vmc(thread, addr);
+	if (!vmc) goto abort;
+
+	union ptel pte;
+	uval laddr = vmc_xlate(vmc, addr, &pte);
+	if (laddr == INVALID_LOGICAL_ADDRESS) goto abort;
+
+	uval pa = logical_to_physical_address(thread->cpu->os, laddr,
+					      sizeof(uval32));
+	if (pa == INVALID_PHYSICAL_ADDRESS) goto abort;
+
+	ins = *(uval32*)pa;
+
+	ret = decode_spr_ins(thread, addr, ins);
+	if (ret == 0) return -1;
+abort:
+	return insert_exception(thread, EXC_V_DEBUG);
+}
+
+
+extern sval deliver_async_exception(struct cpu_thread *thread);
+
+sval
+deliver_async_exception(struct cpu_thread *thread)
+{
+	uval *mode = &thread->vstate.thread_mode;
+	if (test_bit(*mode, VSTATE_PENDING_EXT)) {
+		clear_bit(*mode, VSTATE_PENDING_EXT);
+		return insert_exception(thread, EXC_V_EXT);
+	}
+	if (test_bit(*mode, VSTATE_PENDING_DEC)) {
+		clear_bit(*mode, VSTATE_PENDING_DEC);
+		return insert_exception(thread, EXC_V_DEC);
+	}
+
+	return -1;
+}
+
+
+sval
+insert_exception(struct cpu_thread *thread, uval exnum)
+{
+	struct thread_control_area *tca = get_tca();
+	struct vregs* vregs = tca->vregs;
+	struct vexc_save_regs* vr = tca->save_area;
+
+
+//	hprintf("Reflect exception: %ld %x\n", exnum, vr->exc_num);
+	if (!thread->vregs->exception_vectors[exnum])
+		return vregs->active_vsave;
+
+
+//	hprintf("%s from 0x%016lx %lx\n", __func__, tca->srr0, exnum);
+
+	vr->v_srr0 = tca->srr0;
+
+	assert(tca->srr0 != 0xf000000000000000, "bad reflection");
+
+
+	/* Provide accurate trap bits */
+	vr->v_srr1 = vregs->v_msr | (MSR_TRAP_BITS & tca->srr1);
+	assert(vr->v_srr1 != 0x2900, "bad msr\n");
+	
+	/* enforce HV, EE, PR off, AM on */
+	uval vmsr = vregs->v_msr;
+
+	vmsr &= ~(MSR_EE|MSR_HV|MSR_PR|MSR_IR|MSR_DR|MSR_FE0|
+		  MSR_FE1|MSR_BE|MSR_FP|MSR_PMM|MSR_SE|MSR_BE);
+	vmsr |= MSR_SF;
+//	hprintf("v_msr: 0x%016lx v_ex_msr: 0x%016lx %ld\n",
+//		vregs->v_msr, vmsr, exnum);
+	set_v_msr(thread, vmsr);
+
+	uval64 msr = vregs->v_ex_msr;
+	msr |= V_LPAR_MSR_ON;
+	msr &= ~V_LPAR_MSR_OFF;
+	tca->srr1 = msr;
+	tca->srr0 = vregs->exception_vectors[exnum];
+	
+	/* Make vregs->v_dec match current dec value */
+	sync_from_dec();
+
+
+	vr->prev_vsave = vregs->active_vsave;
+	vregs->active_vsave = (vr - &vregs->vexc_save[0]);
+
+	return vregs->active_vsave;
+}
+
+
+sval
+insert_debug_exception(struct cpu_thread *thread, uval dbgflag)
+{
+	struct thread_control_area *tca = get_tca();
+
+	if (thread->vregs->debug & V_DEBUG_FLAG) {
+
+		thread->vregs->debug |= dbgflag;
+
+		/* Increment to the next instruction */
+		tca->srr0 += sizeof(uval32);
+
+		return thread->vregs->active_vsave;
+	}
+
+	return insert_exception(thread, EXC_V_DEBUG);
+
+}
+
